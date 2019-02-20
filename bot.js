1a550548100feac582d4073a555e0946fd828fd7
jnfevsorvnm <40556229+jnfevsorvnm@users.noreply.github.com>
2019-02-20 16:32:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,89 @@ const fs = require('fs');
 const moment = require('moment');
 
 
+    client.on('message', message => {
+        if (message.content.startsWith("-avatar")) {
+            var mentionned = message.mentions.users.first();
+        var x5bzm;
+          if(mentionned){
+              var x5bzm = mentionned;
+          } else {
+              var x5bzm = message.author;
+               
+          }
+            const embed = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setImage(`${x5bzm.avatarURL}`)
+          message.channel.sendEmbed(embed);
+        }
+    });
+
+
+
+
+    client.on("message", message => {
+        const prefix = "-"
+                   
+              if(!message.channel.guild) return;
+       if(message.author.bot) return;
+          if(message.content === prefix + "image"){ 
+              const embed = new Discord.RichEmbed()
+       
+          .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+      .setAuthor(message.author.username, message.guild.iconrURL)
+        .setColor(0x164fe3)
+        .setImage(message.guild.iconURL)
+        .setURL(message.guild.iconrURL)
+                        .setTimestamp()
+     
+       message.channel.send({embed});
+          }
+      });
+
+
+client.on('message', message => {
+    var prefix = "-";
+    var args = message.content.substring(prefix.length).split(" ");
+    if (message.content.startsWith(prefix + "id")) {
+    var embed = new Discord.RichEmbed()
+    .setColor(0x00A2E8)
+    .setThumbnail(message.author.avatarURL)
+    .addField("اسمك في السيرفر ", `${message.author.tag} (ID: ${message.author.id})`, true)
+    .addField("حالتك :", message.member.presence !== null && message.member.presence.status !== null ? message.member.presence.status : "Offline")
+    .addField("يلعب؟ : ", `${message.author.presence.game === null ? "None" :  message.author.presence.game.name}`, true)
+    .addField("اسمك في دسكورد ", `${message.member.displayName}`, true)
+    .addField("الرتب الذي تمتلكها ", `${message.member.roles.map(r => r.name).join(", ")}`)
+    .addField("أعلى رتبه تمتلكها: ", message.member.highestRole.name)
+    .addField("دخولك للدسكورد :", `${message.member.joinedAt.toDateString()}`)
+    .addField("دخولك للسيرفر : ", `${message.author.createdAt.toDateString()}`)
+    .setTimestamp()
+    .setFooter(message.author.username, message.author.avatarURL);
+  if (message.mentions.users.size < 1) return message.channel.send({ embed: embed });
+    
+  let member = message.mentions.members.first();
+  var embed = new Discord.RichEmbed()
+    .setColor(0x00A2E8)
+    .setThumbnail(member.user.avatarURL)
+    .addField("اسمه في دسكورد:", `${member.user.tag} (ID: ${member.id})`, true)
+    .addField("حالته :", member.presence !== null && member.presence.status !== null ? member.presence.status : "Offline")
+    .addField("يلعب :", `${member.user.presence.game === null ? "Nothing" :  member.user.presence.game.name}`, true)
+    .addField("اسمه في السيرفر:", `${member.nickname === null ? "None" : member.nickname}`, true)
+    .addField("الرتب :", `${member.roles.map(r => r.name).join(", ")}`)
+    .addField("اعلى رتبه يمتلكها : :", member.highestRole.name)
+    .addField("دخوله لدسكورد :", `${member.joinedAt.toDateString()}`)
+    .addField("دخوله للسيرفر", `${member.user.createdAt.toDateString()}`)
+    .setTimestamp()
+    .setFooter(member.user.username, member.user.avatarURL);
+    message.channel.send({ embed: embed });
+            }
+  });
+
+
+
+
+
+
+
 
 
 
