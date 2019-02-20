2bbae8faa899feff5bb05831f823d672d6d25fb1
LEGEND511 <39378632+LEGEND511@users.noreply.github.com>
2019-02-20 22:55:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,11 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
-const client = new Discord.Client();   
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { Client, Util } = require('discord.js');
+const dateFormat = require('dateformat');
+const fs = require('fs');
+const moment = require('moment');
 const giphy = require('giphy-api')();    
-const googl = require('goo.gl'); 
-const translate = require('google-translate-api'); 
-const fs = require("fs");      
-const getYoutubeID = require('get-youtube-id'); 
-const moment = require("moment");  
-const { Client, Util } = require('discord.js');  
-const UserBlocked = new Set();   
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const queue = new Map();
-const zalgo = require('zalgolize');   
-const ytdl = require('ytdl-core');
-const sql = require("sqlite");
- const dateFormat = require('dateformat');
- const pretty = require('pretty-ms') 
-,ti={}  
-,spee={};
- 
-
+const db = require('quick.db');
 
 
 
