e64d8d507680b19075afdfd900b5633d020fa355
jnfevsorvnm <40556229+jnfevsorvnm@users.noreply.github.com>
2019-02-20 16:29:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { Client, Util } = require('discord.js');
+const dateFormat = require('dateformat');
+const fs = require('fs');
+const moment = require('moment');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  client.login(process.env.BOT_TOKEN);
